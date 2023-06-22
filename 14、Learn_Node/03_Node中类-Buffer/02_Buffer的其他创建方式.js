4cd44e2300bad7ab58e9183c10b5c6fe5de4358b
jiumingmao <879021430@qq.com>
2023-06-22 21:10:16 +0800

    change node buffer

--- 14、Learn_Node/03_Node中类-Buffer/02_Buffer的其他创建方式.js
+++ 14、Learn_Node/03_Node中类-Buffer/02_Buffer的其他创建方式.js
@@ -1,19 +1,19 @@
-const fs = require('fs')
+const fs = require("fs");
 
 // 1.创建一个Buffer对象
 // 8个字节大小的buffer内存空间
-const buf = Buffer.alloc(8)
-// console.log(buf)
+const buf = Buffer.alloc(8);
+console.log(buf); // <Buffer 00 00 00 00 00 00 00 00>
 
 // 2.手动对每个字节进行访问
-// console.log(buf[0])
-// console.log(buf[1])
+console.log(buf[0]);
+console.log(buf[1]);
 
 // 3.手动对每个字节进行操作
-buf[0] = 100
-buf[1] = 0x66
-console.log(buf)
-console.log(buf.toString())
+buf[0] = 100;
+buf[1] = 0x66;
+console.log(buf);
+console.log(buf.toString());
 
-buf[2] = 'm'.charCodeAt()
-console.log(buf)
+buf[2] = "m".charCodeAt();
+console.log(buf);
