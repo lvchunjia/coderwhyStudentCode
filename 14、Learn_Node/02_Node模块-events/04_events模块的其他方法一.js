da395da0fda8ccc6938f00f35bad90e6d8019939
jiumingmao <879021430@qq.com>
2023-06-22 20:24:22 +0800

    change node event

--- 14、Learn_Node/02_Node模块-events/04_events模块的其他方法一.js
+++ 14、Learn_Node/02_Node模块-events/04_events模块的其他方法一.js
@@ -1,23 +1,20 @@
-const EventEmitter = require('events')
+const EventEmitter = require("events");
+const ee = new EventEmitter();
 
-const ee = new EventEmitter()
-
-ee.on("why", () => {})
-ee.on("why", () => {})
-ee.on("why", () => {})
-
-ee.on('kobe', () => {})
-ee.on('kobe', () => {})
+ee.on("why", () => {});
+ee.on("why", () => {});
+ee.on("why", () => {});
+ee.on("kobe", () => {});
+ee.on("kobe", () => {});
 
 // 1.获取所有监听事件的名称
-console.log(ee.eventNames())
+console.log(ee.eventNames());
 
 // 2.获取监听最大的监听个数
-console.log(ee.getMaxListeners())
+console.log(ee.getMaxListeners());
 
 // 3.获取某一个事件名称对应的监听器个数
-console.log(ee.listenerCount('why'))
+console.log(ee.listenerCount("why"));
 
 // 4.获取某一个事件名称对应的监听器函数(数组)
-console.log(ee.listeners('why'))
-
+console.log(ee.listeners("why"));
